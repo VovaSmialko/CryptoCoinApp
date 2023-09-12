8352566528dd077c184c66be5a8b5d66d884ec56
vova77 <vova2000smi@gmail.com>
2023-09-12 22:41:19 +0300

    Added Api

--- app/src/main/java/com/example/cryptocoinapp/MainActivity.kt
+++ app/src/main/java/com/example/cryptocoinapp/MainActivity.kt
@@ -2,10 +2,33 @@ package com.example.cryptocoinapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import com.example.cryptocoinapp.api.ApiFactory
+import io.reactivex.rxjava3.android.schedulers.AndroidSchedulers
+import io.reactivex.rxjava3.core.Scheduler
+import io.reactivex.rxjava3.disposables.CompositeDisposable
+import io.reactivex.rxjava3.schedulers.Schedulers
 
 class MainActivity : AppCompatActivity() {
+
+    private val compositeDisposable = CompositeDisposable()
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+       val disposable =  ApiFactory.apiService.getTopCoinsInfo()
+            .subscribeOn(Schedulers.io())
+            .observeOn(AndroidSchedulers.mainThread())
+            .subscribe({
+                Log.d("TEST_OF_LOADING_DATA", it.toString())
+            }, {
+                it.message?.let { it1 -> Log.d("TEST_OF_LOADING_DATA", it1) }
+            })
+        compositeDisposable.add(disposable)
+    }
+
+
+    override fun onDestroy() {
+        super.onDestroy()
+        compositeDisposable.dispose()
     }
 }
---
+++ app/src/main/java/com/example/cryptocoinapp/database/AppDatabase.kt
@@ -0,0 +1,4 @@
+package com.example.cryptocoinapp.database
+
+class AppDatabase {
+}
